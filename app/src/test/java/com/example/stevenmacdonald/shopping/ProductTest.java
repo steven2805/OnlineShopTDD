a1415d6b52695364434d797573e15abc24af5976
steven2805 <stevenmac90@googlemail.com>
2017-07-21 13:42:50 +0100

    two passing tests

--- app/src/test/java/com/example/stevenmacdonald/shopping/ProductTest.java
+++ app/src/test/java/com/example/stevenmacdonald/shopping/ProductTest.java
@@ -12,17 +12,22 @@ import static junit.framework.Assert.assertEquals;
 public class ProductTest {
 
 
-    Product product;
+    Product product, product2;
 
     @Before
     public void before(){
         product = new Product("Apples",88.99,false);
-//        product2 = new Product("Grapes", 2.99,true);
+        product2 = new Product("Grapes", 2.99,true);
     }
 
     @Test
     public void testingTheNameOfAnItem(){
         assertEquals("Apples", product.getName());
+    }
+
+    @Test
+    public void testingThePricing(){
+        assertEquals(2.99, product2.getValue());
     }
 
 }
